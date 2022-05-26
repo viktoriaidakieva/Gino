593a00235ca22ee0c7ba6edf45e4ef9bc3ba04c3
“biancanavey” <naveybianca@gmail.com>
2022-05-26 11:24:56 +0100

    adding styling to list

--- gino/src/components/Nav/nav.js
+++ gino/src/components/Nav/nav.js
@@ -8,38 +8,44 @@ import sharedIcon from '../../icons/shared files icon.png';
 import archiveIcon from '../../icons/archive icon.png';
 import recycleBinIcon from '../../icons/recycle bin icon.png';
 import expandIcon from '../../icons/expand icon.png';
-
+import myFoldersIcon from '../../icons/my folders icon.png';
 
 function NavBar () {
-   const [isExpanded, setExpanded] = useState(false)
-   const { getCollapseProps, getToggleProps } = useCollapse({ isExpanded })
+   const config = {
+      hasDisabledAnimation: true,
+      duration: 1
+    };
+   const { getCollapseProps, getToggleProps,isExpanded} = useCollapse(config)
     return (
             <nav className='NavbarItems'>
-               <div className='menu-icon'>
-                 
-                   {/*FIX BUTTON FOR COLLAPSE  */}
-                 
+               <div className='menu-icon' style= {{display:"flex"}}>
                          <section>
                          <button id ="dashboard" title="Dashboard" className="menu-item" href="#dashboard"><img src={dashboardIcon} alt='dashboardIcon'></img></button>
+                         <button id="myFolders" title="My Folders" className="menu-item"  href="#myFolders"><img src={myFoldersIcon} alt='myFoldersIcon'></img></button>
                   <button id="favourites" title="Favourites" className="menu-item"  href="#favourites"><img src={favouritesIcon} alt='favouritesIcon'></img></button>
                   <button id ="recent" title="Recent" className="menu-item"  href="#recent"><img src={recentIcon} alt='recentIcon'></img></button>
                    <button id="shared" title="Shared" className="menu-item"  href="#shared"><img src={sharedIcon} alt='sharedIcon'></img></button>
                   <button id="archive" title="Archive" className="menu-item"  href="#archive"><img src={archiveIcon} alt='archiveIcon'></img></button>
                   <button id="recycleBin" title="Recycle Bin" className="menu-item"  href="#recyleBin"><img src={recycleBinIcon} alt='recycleBinIcon'></img></button>
+                  <button id="expandAndCollapse" title="Expand or Collapse" className="menu-item" {...getToggleProps()}><img src={expandIcon} alt="expandIcon"></img></button>
                          </section>
-                         <section {...getCollapseProps()}>
-                  <li>Dashboard</li>
-                  <li>Favourites</li>
-                  <li>Recents</li>
-                  <li>Shared</li>
-                  <li>Archive</li>
-                  <li>Recycle Bin</li>
-                           </section>
-                           <button id="expandAndCollapse" className="menu-item"{...getToggleProps({
+                         {/* <div style= {{display:"flex"}}> */}
+                         
+                           {/* <button id="expandAndCollapse" title="Expand or Collapse" className="menu-item"{...getToggleProps({
           onClick: () => setExpanded((prevExpanded) => !prevExpanded),
-        })}><img src={expandIcon} alt="expandIcon"></img>  
-                        {isExpanded ? 'Collapse' : 'Expand'}
-                         </button>
+        })}><img src={expandIcon} alt="expandIcon"></img>   */}
+                        {/* {isExpanded ? "Collapse" : "Expand"} */}
+                        
+                        <section {...getCollapseProps()}>
+                           <li id ="dashboard" title="Dashboard" className="menu-item-description" href="#dashboard">Dashboard</li>
+                           <li id="myFolders" title="My Folders" className="menu-item-description"  href="#myFolders">My Folders</li>
+                           <li id="favourites" title="Favourites" className="menu-item-description"  href="#favourites">Favourites</li>
+                           <li id ="recent" title="Recent" className="menu-item-description"  href="#recent">Recents</li>
+                           <li id="shared" title="Shared" className="menu-item-description"  href="#shared">Shared</li>
+                           <li id="archive" title="Archive" className="menu-item-description"  href="#archive">Archive</li>
+                           <li id="recycleBin" title="Recycle Bin" className="menu-item-description"  href="#recyleBin">Recycle Bin</li>
+                           </section>
+                        {/* </div> */}
                </div>
             </nav>
       );
