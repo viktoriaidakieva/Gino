b250681af131bbbf8c2a34dbf162216679c1dffe
viktoriaidakieva <106076275+viktoriaidakieva@users.noreply.github.com>
2022-05-26 11:51:09 +0100

    chore: add sidebar new file button

--- gino/src/App.js
+++ gino/src/App.js
@@ -1,13 +1,26 @@
 import logo from './icons/ginologo.jpg';
 import './App.css';
 import Dashboard from './components/Dashboard/Dashboard';
+import Sidebar from './components/Sidebar/Sidebar';
+
+import Button from '@mui/material/Button';
+import FormatListBulleted from '@mui/icons-material/FormatListBulleted';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       </header>
+      <div className="Button-container">
+      <Button
+        variant="contained"
+        style={{background: "#FDE8E8", color: "#000000", margin: "0px", padding: "2px"}}
+        startIcon={<FormatListBulleted />}></Button>
+      </div>
+      <div className="App-content">
       <Dashboard/>
+      <Sidebar/>
+      </div>
     </div>
   );
 }
